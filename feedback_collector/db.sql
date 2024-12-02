da1b633da931b9f01eb9e229ef5b539431341860
Anupama Balasooriya <anubalasooriya2017@gmail.com>
2024-12-03 00:57:42 +0530

    Updated db commands

--- feedback_collector/db.sql
+++ feedback_collector/db.sql
@@ -1,62 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 29, 2024 at 08:23 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
+CREATE TABLE images (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    path VARCHAR(255) NOT NULL
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `color_picker`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `colors`
---
-
-CREATE TABLE `colors` (
-  `id` int(11) NOT NULL,
-  `primary_color` varchar(7) NOT NULL,
-  `secondary_color` varchar(7) NOT NULL,
-  `top_app_bar` varchar(20) DEFAULT NULL,
-  `bottom_app_bar` varchar(20) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `colors`
---
-ALTER TABLE `colors`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `colors`
---
-ALTER TABLE `colors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE ui_colors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    image_id INT NOT NULL,
+    ui_element VARCHAR(50) NOT NULL,
+    color VARCHAR(20) NOT NULL,
+    FOREIGN KEY (image_id) REFERENCES images(id)
+);
